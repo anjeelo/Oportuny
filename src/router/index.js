eb03a8186b86d7c92c749f03af9bb87d2419b29c
arthurhenriquemidas <166043613+arthurhenriquelopes@users.noreply.github.com>
2026-07-02 19:31:11 -0300

    fix: change Vue Router to Hash Mode for server-independent routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { isAuthenticated } from '../stores/auth'
 
 import OnboardingView from '../views/OnboardingView.vue'
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
